d99e048741997bda48eef3df83b56524b26baa9b
chundh <dhchun0716@gmail.com>
2020-11-26 10:36:00 +0900

    feat : 3자리 랜덤 숫자 생성을 위한 createNumber()추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,26 @@
 package baseball;
 
+import utils.RandomUtils;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        String answer = createNumber();
+    }
+    public static String createNumber(){
+        StringBuilder targetNubmer = new StringBuilder();
+        int[] useNumber = new int[10];
+        while(targetNubmer.length()<3) {
+            int tempNumber = RandomUtils.nextInt(1, 9);
+            if(useNumber[tempNumber]==0){
+                useNumber[tempNumber] = 1;
+                targetNubmer.append(tempNumber);
+            }
+        }
+        System.out.println(targetNubmer.toString());
+        return targetNubmer.toString();
     }
 }
